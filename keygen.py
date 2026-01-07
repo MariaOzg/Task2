128c42221820e950935644c85e2ad879c5c1a7ec
MariaOzg <69230600+MariaOzg@users.noreply.github.com>
2026-01-07 17:50:40 +0300

    Update keygen.py

--- keygen.py
+++ keygen.py
@@ -1,16 +1,1 @@
-import streamlit_authenticator as stauth
-from streamlit_authenticator.utilities.hasher import Hasher
 
-# Ваши пароли
-passwords = [
-    "pass1", 
-    "pass2", 
-    "pass3", 
-    "pass4", 
-    "pass5"
-]
-
-# НОВЫЙ СПОСОБ ГЕНЕРАЦИИ:
-hashed_passwords = Hasher.hash_list(passwords)
-
-print(hashed_passwords)
